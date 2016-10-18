ec62d67a075b4e0ca3baa05b0e44c11c74aed9da
Nick Johnstone <ncwjohnstone@gmail.com>
2016-10-19 11:37:31 +1300

    Add a missing semicolon and fix eslint warning

--- template/src/app.js
+++ template/src/app.js
@@ -1,5 +1,5 @@
 import {div} from '@cycle/dom';
---IMPORT--
+--IMPORT--;
 
 function App (sources) {
   return {
--- template/test/app-test.js
+++ template/test/app-test.js
@@ -1,3 +1,4 @@
+/* globals describe, it */
 const assert = require('assert');
 const App = require('../src/app');
 
